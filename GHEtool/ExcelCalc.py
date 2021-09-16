cd07095dbf797ec20cd85eaeac19ca6c377759f2
Pedro <pedromarques.be@pm.me>
2021-09-16 11:08:22 +0200

    full integration of excel in python

--- GHEtool/ExcelCalc.py
+++ GHEtool/ExcelCalc.py
@@ -4,34 +4,41 @@ import xlwings as xw
 import numpy as np
 
 if __name__ == "__main__":
-    # relevant borefield data for the calculations
-    data = {"H": 110,           # depth (m)
-            "B": 6,           # borehole spacing (m)
-            "k_s": 3.5,           # conductivity of the soil (W/mK)
-            "Tg":10,            # Ground temperature at infinity (degrees C)
-            "Rb":0.15,           # equivalent borehole resistance (K/W) 0.08-0.18
-            "N_1":12,           # width of rectangular field (#)
-            "N_2":10}           # length of rectangular field (#)}
-
-    HeatingDemand = np.arange(12)
-    CoolingDemand = np.arange(12)
-    
     sheet = xw.sheets[0]
     
+    # relevant borefield data for the calculations
+    data = {"H": sheet['bh_depth'].value,           # depth (m)
+            "B": sheet['bh_spacing'].value,           # borehole spacing (m)
+            "N_1":int(sheet['bh_width'].value),           # width of rectangular field (#)
+            "N_2":int(sheet['bh_length'].value),           # length of rectangular field (#)}
+            "Rb":sheet['bh_resistance'].value,           # equivalent borehole resistance (K/W) 0.08-0.18
+            "Tg":sheet['soil_temperature'].value,            # Ground temperature at infinity (degrees C)
+            "k_s": sheet['soil_conductivity'].value,           # conductivity of the soil (W/mK)
+            }
+    HeatingDemand = np.arange(12)
+    HeatingPeak = np.arange(12)
+    
+    CoolingDemand = np.arange(12)
+    CoolingPeak = np.arange(12)
+    
     for i in range(0, 12):
-        HeatingDemand[i] = sheet['heating'].offset(i + 1,0).value        
-        CoolingDemand[i] = sheet['cooling'].offset(i + 1,0).value        
+        HeatingDemand[i] = sheet['heating_demand'].offset(i + 1,0).value
+        CoolingDemand[i] = sheet['cooling_demand'].offset(i + 1,0).value
+        HeatingPeak[i] = sheet['heating_peak'].offset(i + 1,0).value
+        CoolingPeak[i] = sheet['cooling_peak'].offset(i + 1,0).value
 
     # Montly loading values
-    peakCooling = [0., 0, 34., 69., 133., 187., 213., 240., 160., 37., 0., 0.]              # Peak cooling in kW
-    peakHeating = [160., 142, 102., 55., 0., 0., 0., 0., 40.4, 85., 119., 136.]             # Peak heating in kW
+    #peakCooling = [0., 0, 34., 69., 133., 187., 213., 240., 160., 37., 0., 0.]              # Peak cooling in kW
+    #peakHeating = [160., 142, 102., 55., 0., 0., 0., 0., 40.4, 85., 119., 136.]             # Peak heating in kW
 
     # annual heating and cooling load
     print ("Heating demand: %i kWh"%HeatingDemand.sum())
     print ("Cooling demand: %i kWh"%CoolingDemand.sum())
     annualHeatingLoad = HeatingDemand.sum()
     annualCoolingLoad = CoolingDemand.sum()
-
+    
+    
+    
     # percentage of annual load per month (15.5% for January ...)
     #print ("Monthly heating demand:")
     #print (HeatingDemand / HeatingDemand.sum())
@@ -45,8 +52,8 @@ if __name__ == "__main__":
     # create the borefield object
 
     borefield = ghe.Borefield(simulationPeriod=20,
-                          peakHeating=peakHeating,
-                          peakCooling=peakCooling,
+                          peakHeating=HeatingPeak,
+                          peakCooling=CoolingPeak,
                           baseloadHeating=monthlyLoadHeating,
                           baseloadCooling=monthlyLoadCooling)
 
@@ -56,67 +63,19 @@ if __name__ == "__main__":
     borefield.setMaxGroundTemperature(16)   # maximum temperature
     borefield.setMinGroundTemperature(0)    # minimum temperature
 
+    print ("Imbalance: %i kWh"%borefield.imbalance) # print imbalance
+
     # size borefield
     depth= borefield.size(100)
     print ("Optimized depth: %.1f m"%depth)
-
-    #print imbalance
-    print (borefield.imbalance) # print imbalance
+    sheet['results_depth'].value = depth
 
     # plot temperature profile for the calculated depth
-    borefield.printTemperatureProfile(legend=True)
+    #borefield.printTemperatureProfile(legend=True)
 
     #plot temperature profile for a fixed depth
-    borefield.printTemperatureProfileFixedDepth(depth=75,legend=False)
+    borefield.printTemperatureProfileFixedDepth(depth=data['H'],legend=False)
 
     # print gives the array of montly tempartures for peak cooling without showing the plot
     borefield.calculateTemperatures(depth=90)
     #print (borefield.resultsPeakCooling)
-
-    ### Custom field
-    """When working on a custom borefield configuration, one needs to import this configuration into the GHEtool.
-    Based on the pygfunction, one creates his custom borefield and gives it as an argument to the class initiater Borefield of GHEtool.
-    
-    You also need a custom g-function file for interpolation. This can also be given as an argument to the class initiater as customGfunction.
-    This custom variable, must contain gfunctions for all time steps in Borefield.defaultTimeArray, and should be structured as follows:
-    {"Time":Borefield.defaultTimeArray,"Data":[[Depth1,[Gfunc1,Gfunc2 ...]],[Depth2,[Gfunc1, Gfunc2 ...]]]}.
-    
-    However, one can use the function 'createCustomDataset' when a custom borefield is given. This will make the required dataset for the optimisation.
-    Please note that, depending on the complexity of the custom field, this can range between 5 minutes and 5 hours. 
-    """
-
-    # Montly loading values
-    # peakCooling = [0., 0, 3.4, 6.9, 13., 18., 21., 50., 16., 3.7, 0., 0.]  # Peak cooling in kW
-    # peakHeating = [60., 42., 10., 5., 0., 0., 0., 0., 4.4, 8.5, 19., 36.]  # Peak heating in kW
-
-    # # annual heating and cooling load
-    # annualHeatingLoad = 30 * 10 ** 3  # kWh
-    # annualCoolingLoad = 16 * 10 ** 3  # kWh
-
-    # # percentage of annual load per month (15.5% for January ...)
-    # montlyLoadHeatingPercentage = [0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, 0.117, 0.144]
-    # montlyLoadCoolingPercentage = [0.025, 0.05, 0.05, .05, .075, .1, .2, .2, .1, .075, .05, .025]
-
-    # # resulting load per month
-    # monthlyLoadHeating = list(map(lambda x: x * annualHeatingLoad, montlyLoadHeatingPercentage))  # kWh
-    # monthlyLoadCooling = list(map(lambda x: x * annualCoolingLoad, montlyLoadCoolingPercentage))  # kWh
-
-    # # create the borefield object
-
-    # borefield = Borefield(simulationPeriod=20,
-    #                       peakHeating=peakHeating,
-    #                       peakCooling=peakCooling,
-    #                       baseloadHeating=monthlyLoadHeating,
-    #                       baseloadCooling=monthlyLoadCooling)
-
-    # borefield.setGroundParameters(data)
-
-    # # set temperature boundaries
-    # borefield.setMaxGroundTemperature(16)  # maximum temperature
-    # borefield.setMinGroundTemperature(0)  # minimum temperature
-
-    # customField = gt.boreholes.L_shaped_field(N_1=3, N_2=3, B_1=5., B_2=5.,H=100., D=4, r_b=0.05)
-    # borefield.createCustomDataset(customField,"customField")
-    # borefield.setCustomGfunction("customfield")
-    # borefield.setBorefield(customField)
-    # borefield.printTemperatureProfileFixedDepth(100)
